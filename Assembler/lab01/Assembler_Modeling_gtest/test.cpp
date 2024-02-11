f3d42d5beda34bcef97742696f460dad828cf8c7
Yaroslavcoder47 <karacuna396@gmail.com>
2024-02-11 21:36:02 +0300

    commit -m "start testing"

--- Assembler/lab01/Assembler_Modeling_gtest/test.cpp
+++ Assembler/lab01/Assembler_Modeling_gtest/test.cpp
@@ -1,6 +1,30 @@
 #include "pch.h"
+#include "../Modeling_Assembler/ProgramClass.h"
+
+class ProgramTest : public ::testing::Test
+{
+protected:
+	void SetUp() {
+		obj = new ProgramClass;
+	}
+	void TearDown() {
+		delete obj;
+	}
+	ProgramClass* obj;
+};
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST_F(ProgramTest, SetGetRegData) {
+	obj->setRegisterData(10);
+	EXPECT_EQ(obj->getRegisterData(), 10);
+}
+
+TEST_F(ProgramTest, SetGetResData) {
+	obj->setResultData("first", 10);
+	EXPECT_EQ(obj->getResultData("first"), 10);
+}
+
