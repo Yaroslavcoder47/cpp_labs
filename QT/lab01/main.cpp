32f4c19cfa89407bb73b9d14e8d63ee271355fa3
Yaroslavcoder47 <karacuna396@gmail.com>
2024-02-15 20:38:47 +0300

    First task

--- QT/lab01/main.cpp
+++ QT/lab01/main.cpp
@@ -1,8 +1,24 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <QTextStream>
 
-int main(int argc, char *argv[])
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    QTextStream in(stdin);
+    QTextStream out(stdout);
+    QString str1, str2;
+    in >> str1;
+    in >> str2;
 
-    return a.exec();
+    for(int i = 0; i < str1.length(); ++i){
+        if(str2.toLower().contains(str1.toLower()[i])){
+            out << "yes ";
+        }
+        else{
+            out << "no ";
+        }
+    }
+    out << Qt::endl;
+    return 0;
 }
