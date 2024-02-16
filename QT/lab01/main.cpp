3abb0e5cf49087bc645145002e4a3590209521d9
Yaroslavcoder47 <karacuna396@gmail.com>
2024-02-16 10:35:47 +0300

    second task

--- QT/lab01/main.cpp
+++ QT/lab01/main.cpp
@@ -26,26 +26,33 @@ void f1()
     out << Qt::endl;
 }
 
-int main()
+void f2()
 {
     int counter1 = 0;
     int counter2 = 0;
     QTextStream out(stdout);
-    QTextStream in(stdin);
-    //QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf-8"));
     out.setCodec(QTextCodec::codecForName("cp866"));
-    //in.setCodec(QTextCodec::codecForName("cp866"));
+
+    QTextStream in(stdin);
+    in.setCodec(QTextCodec::codecForName("cp866"));
+
     QString str = in.readLine();
     QStringList list = str.split(' ');
+
     foreach (QString word, list){
-        if(word[0] == 'н'){
-            counter1++;
+        if(word[0] == "н"){
+            ++counter1;
         }
-        if(word[word.length()-1] == 'р'){
-            counter2++;
+        if(word[word.length()-1] == "р"){
+            ++counter2;
         }
     }
-    out << "Количество слов, начинающихся с н: " << counter1 << Qt::endl;
-    out << "Количество слов, заканчивающихся на р: " << counter2 << Qt::endl;
+    out << QString("Количество слов, начинающихся с н: ") << counter1 << Qt::endl;
+    out << QString("Количество слов, заканчивающихся на р: ") << counter2 << Qt::endl;
+}
+
+int main()
+{
+    f2();
     return 0;
 }
