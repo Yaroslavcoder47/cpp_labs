9d5c8c87bba00f565651e3f8a09a27488b759016
Yaroslavcoder47 <karacuna396@gmail.com>
2024-03-06 10:48:29 +0300

    set alligment to elements

--- QT/Lab03/Translator/translator.cpp
+++ QT/Lab03/Translator/translator.cpp
@@ -1,23 +1,22 @@
 #include "translator.h"
 #include "ui_translator.h"
-#include <iostream>
 
 Translator::Translator(QWidget *parent) : QWidget(parent)
 {
-   // ui->setupUi(this);
-    QLabel *result = new QLabel("jgjthgjtgjtghjtgh", this);
+    QLabel *resultLabel = new QLabel("0", this);
+    QLabel *infoLabel1 = new QLabel("Input integer decimal number", this);
+    QLabel *infoLabel2 = new QLabel("Result as binary number", this);
     QLineEdit *number = new QLineEdit(this);
-    number->frameSize();
 
-    QVBoxLayout *grid = new QVBoxLayout();
-    grid->setContentsMargins(50, 50, 50, 100);
-    grid->setSpacing(20);
-    std::cout << grid->spacing();
-    grid->setAlignment(Qt::AlignHCenter);
-    grid->addWidget(number);
-    grid->addWidget(result);
+    QVBoxLayout *box = new QVBoxLayout();
+    box->setContentsMargins(50, 50, 50, 100);
+    box->addWidget(infoLabel1);
+    box->addWidget(number);
+    box->addWidget(infoLabel2);
+    box->addWidget(resultLabel);
 
-    setLayout(grid);
+    //box->setAlignment(Qt::AlignHCenter);
 
+    setLayout(box);
 }
 
