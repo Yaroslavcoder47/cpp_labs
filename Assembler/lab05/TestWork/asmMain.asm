232b34c73350557526906c03c7da839683763e1f
Yaroslavcoder47 <karacuna396@gmail.com>
2024-04-10 22:52:50 +0300

    commit -m "start lab05"

--- Assembler/lab05/TestWork/asmMain.asm
+++ Assembler/lab05/TestWork/asmMain.asm
@@ -1,31 +1,9 @@
 .686
-PUBLIC @mul2fastCall@4
+PUBLIC @palindrome@12
 .model flat
 .code
-@mul2fastCall@4 proc
- mov eax,ecx ;первый параметр
- mov ebx, 2 ;второй параметр
- cdq
- imul ebx
- ret
-@mul2fastCall@4 endp
-end
+@palindrome@12 proc
+	lea ebp, ecx
 
 
-PUBLIC @mul2StdCall@4
-.model flat
-.code
-@mul2StdCall@4 proc
- mov eax,ecx ;первый параметр
- mov ebx, 2 ;второй параметр
- cdq
- imul ebx
- ret
-@mul2StdCall@4 endp
-end
 
-PUBLIC @palindromfastCall@8
-.model flat
-.code
-@palindromfastCall@8
-	mov 
--- Assembler/lab05/TestWork/main.cpp
+++ Assembler/lab05/TestWork/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-extern "C" int __fastcall mul2fastCall(int);
-extern "C" int __stdcall mul2StdCall(int);
-int main() {
 
-	std::cout << "multipled by 2 = " << mul2(10) << std::endl;
+extern "C" bool __fastcall palindrome(char*);
 
+int main()
+{
+	char* str
 }
-
