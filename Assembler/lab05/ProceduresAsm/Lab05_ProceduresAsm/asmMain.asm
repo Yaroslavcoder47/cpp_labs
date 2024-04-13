8332412ae02a763ce0227ea6bbfb3cbae88b784a
Yaroslavcoder47 <karacuna396@gmail.com>
2024-04-13 18:12:24 +0300

    commit -m "start writing func"

--- Assembler/lab05/ProceduresAsm/Lab05_ProceduresAsm/asmMain.asm
+++ Assembler/lab05/ProceduresAsm/Lab05_ProceduresAsm/asmMain.asm
@@ -1,1 +1,27 @@
+.686
+PUBLIC _Palindrome@8
+.model flat
+.code
+_Palindrome@8 proc
+
+push ebp
+mov ebp, esp
+mov esi, [ebp+8]
+mov ecx, [ebp+12]
+dec ecx
+mov edx, 0
+checkLoop_:
+	cmp ecx, edx
+	jl end_
+	mov eax, [esi + ecx]
+	mov ebx, [esi + edx]
+	cmp eax, ebx
+	jne end_
+	inc edx
+	dec ecx
+	jmp checkLoop_
+end_:
+pop ebp
+ret 8
+_Palindrome@8 endp
 end
--- Assembler/lab05/ProceduresAsm/Lab05_ProceduresAsm/main.cpp
+++ Assembler/lab05/ProceduresAsm/Lab05_ProceduresAsm/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+extern "C" int __stdcall  Palindrome(char*, int);
+
 int main()
-{
+{	
+	int size;
+	std::cout << "Enter size: ";
+	std::cin >> size;
+	std::cout << "Enter string:\n";
+	char st[20];
+	std::cin >> st;
+	std::cout << Palindrome(st, size);
 	return 0;
 }
