f8498f6bdab9bb56e7bc79c8a904a1026f957548
Yaroslavcoder47 <karacuna396@gmail.com>
2024-04-22 20:22:38 +0300

    commit -m "last work"

--- KR_Asm/Task1/main.cpp
+++ KR_Asm/Task1/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <utility>
 #include <cmath>
+#include <vector>
 
 extern "C" float __fastcall calculateSqrt(int, float*);
+extern "C" void __cdecl CreateVectorB(unsigned int**, int n, int m, int* b);
 
 float CalculateSqrt(int n, float* a) {
     float variable = *a;
@@ -33,7 +35,7 @@ float CalculateSqrt(int n, float* a) {
     return *a;
 }
 
-int main() {
+void task1() {
     std::cout << "Input value of number a = ";
     float a;
     std::cin >> a;
@@ -41,17 +43,56 @@ int main() {
     int n;
     std::cin >> n;
 
-    float result = CalculateSqrt(n, &a);
+    //float result = CalculateSqrt(n, &a);
 
-    /*float* aPtr = &a;
+    float* aPtr = &a;
     float result;
     __asm {
         mov ecx, n
         mov edx, dword ptr[aPtr]
         call calculateSqrt
         fstp dword ptr[result]
-    }*/
+    }
 
     std::cout << "Result of function: " << result << ' ' << "Difference: " << std::sqrt(a) - result;
+}
+
+int main() {
+    
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> res(n);
+    int* b = res.data();
+    unsigned int** a = new unsigned int* [n];
+
+    for (size_t i = 0; i < n; ++i) {
+        a[i] = new unsigned int[m];
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < m; ++j) {
+            std::cin >> a[i][j];
+        }
+    }
+    CreateVectorB(a, n, m, b); 
+    /*__asm {
+        push n
+        push m
+        push dword 
+       
+        xor eax, eax
+        xor ebx, ebx
+        call CreateVectorB
+    }*/
+    /*for (int u = 0; u < n; ++u) {
+        for (int i = 0; i < m; ++i) {
+
+            for (int j = i + 1; j < m; ++j) {
+                if (a[u][i] == a[u][j]) {
+
+                }
+            }
+        }
+    }*/
     return 0;
 }
