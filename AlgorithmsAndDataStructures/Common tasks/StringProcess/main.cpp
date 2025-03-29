e218e13c67fec204f5abf73d08c774bf413351fb
karachunYaraslau <karacuna396@gmail.com>
2025-03-29 12:26:02 +0300

    f

--- AlgorithmsAndDataStructures/Common tasks/StringProcess/main.cpp
+++ AlgorithmsAndDataStructures/Common tasks/StringProcess/main.cpp
@@ -6,5 +6,13 @@ using namespace std;
 
 int main()
 {
+    ifstream fin("in.txt");
+    ofstream fout("out.txt");
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    string a, b;
+    cin >> a;
+    cin >> b;
     return 0;
 }
